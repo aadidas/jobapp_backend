7e93e7355b6a9f58de4ba2c11973c2683c88f86d
aadityadas <aaditya.das@diginnovators.com>
2024-05-20 22:04:11 +0530

    added chat model

---
+++ models/Chat.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const chatSchema = mongoose.Schema({
+    chatName: {type: String, trim: true},
+    isGroup: {type: Boolean, default: false},
+    users: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        }
+    ],
+    latestMessage: {
+        type: mongoose.Schema.ObjectId,
+        ref: "Message",
+    },
+    groupAdmin: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+}, {timestamp: true});
+
+
+module.exports = mongoose.model("Chat", chatSchema);
